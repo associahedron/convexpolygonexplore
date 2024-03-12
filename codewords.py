96426bee331c3d71416d058d0401119f0d1d6767
Chris Tralie <chris.tralie@gmail.com>
2024-03-12 11:55:22 -0400

    Marking nodes whose i-suffixes are extreme

--- codewords.py
+++ codewords.py
@@ -9,6 +9,26 @@ class Codeword:
             self.w[0] = N-1
         else:
             self.w = np.array(w, dtype=int)
+    
+    def get_num_extreme(self):
+        """
+        Return the number of "length i suffixes" (indices 0:i) of this codeword 
+        that are in extreme form, i <= 2 <= n (def pg. 15)
+        """
+        w = self.w
+        n = w.size
+        is_extreme = w[0] == n-1-np.sum(w[1:])
+        num = 0
+        k = 1
+        while k < n and is_extreme:
+            is_extreme = is_extreme and (w[k] == 0 or w[k] == n-k-np.sum(w[k+1:]))
+            if is_extreme:
+                num += 1
+            k += 1
+        return num
+
+    def is_rotation_neighbor(self, other):
+        pass
 
     def draw(self, d, c, options=None):
         """
@@ -23,9 +43,8 @@ class Codeword:
                 If True, show codeword values at vertices (default True)
             min_idx: int
                 Minimum index from which to draw edges or numbers
-            bold_first: bool
-                If true, draw the first index in red (useful for stacks).
-                Default False
+            bold_idxs: set
+                Bold the indices in this set (useful for stacks)
         }
         """
         if not options:
@@ -34,8 +53,8 @@ class Codeword:
             options["show_codeword"] = True
         if not "min_idx" in options:
             options["min_idx"] = 0
-        if not "bold_first" in options:
-            options["bold_first"] = False
+        if not "bold_idxs" in options:
+            options["bold_idxs"] = set([])
 
         min_idx = options["min_idx"]
         ## Step 1: Draw polygon boundary
@@ -51,7 +70,7 @@ class Codeword:
             for i in range(N):
                 if i >= min_idx:
                     c = 'k'
-                    if i == min_idx and options["bold_first"]:
+                    if i in options["bold_idxs"]:
                         c = 'r'
                     xi = X[i, 0] + r*0.2*np.cos(theta[i])
                     yi = X[i, 1] + r*0.2*np.sin(theta[i])
@@ -77,7 +96,7 @@ class Codeword:
             i -= 1
         plt.axis("off")
 
-def make_stack_rec(w, d, y_offset, opts):
+def make_stack_rec(w, d, y_offset, opts, order=0):
     n = w.size
     diam = opts["diameter"]
     h = w.size-d-np.sum(w[d+1:])+1
@@ -87,23 +106,36 @@ def make_stack_rec(w, d, y_offset, opts):
     plt.text(x_offset-diam/2, y_offset+1.2*diam/2, title)
     y1 = y_offset+1.4*diam/2
     n_items = 0
-    for i in range(h):
+    vals = list(range(h))
+    if order%2 == 1:
+        vals = reversed(vals)
+    max_extreme = 0
+    for val in vals:
         wi = np.array(w)
-        wi[d] = i
+        wi[d] = val
+        n_extreme = 0
         if d == 1:
             ## Base case
             wi[0] = n-1-np.sum(wi[1:])
             ni = 1
             c = Codeword(wi)
-            c.draw(diam, np.array([x_offset, y_offset]))
-            plt.text(x_offset+diam*1.2/2, y_offset, "{}".format(wi))
+            c.draw(diam, np.array([x_offset, y_offset]), {
+                "bold_idxs":set([1])
+            })
+            s = "{}".format(wi)
+            n_extreme = c.get_num_extreme()
+            plt.text(x_offset+diam*1.2/2, y_offset, s)
         else:
             c = Codeword(wi)
             c.draw(diam, np.array([x_offset, y_offset]), {
                 "min_idx":d,
-                "bold_first":True
+                "bold_idxs":set([d])
             })
-            ni = make_stack_rec(wi, d-1, y_offset, opts)
+            ni, n_extreme = make_stack_rec(wi, d-1, y_offset, opts, order)
+            #order += 1
+        max_extreme = max(max_extreme, n_extreme)
+        if n_extreme >= d:
+            plt.scatter(x_offset, y_offset, 200, c='r', marker='x')
         n_items += ni
         y_offset -= diam*1.5*ni
     y2 = y_offset + 1.4*diam/2
@@ -112,7 +144,7 @@ def make_stack_rec(w, d, y_offset, opts):
     if d == 1:
         x2 += 0.7*diam
     plt.plot([x1, x1, x2, x2, x1], [y1, y2, y2, y1, y1], c='k')
-    return n_items
+    return n_items, max_extreme
 
 
 def make_stack(n, opts=None):
